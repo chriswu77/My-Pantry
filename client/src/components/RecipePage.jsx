aaec5f43fc83e29f360efd099d6c21c9b4c2c9a7
Chris Wu <cwu0569@gmail.com>
2021-09-30 01:59:01 -0700

    fixed on mount logic for recipe

--- client/src/components/RecipePage.jsx
+++ client/src/components/RecipePage.jsx
@@ -36,107 +36,104 @@ const RecipePage = () => {
   const [selectRecipe, setSelectRecipe] = useState();
 
   useEffect(async () => {
-    if (recipes.length > 0) {
-      const foundRecipe = recipes.find((recipe) => recipe.id === Number(id));
+    const foundRecipe = recipes.find((recipe) => recipe.id === Number(id));
 
-      if (foundRecipe) {
-        setSelectRecipe(foundRecipe);
-      } else {
-        const results = await axios.get(`/api/recipes/${id}`);
-        setSelectRecipe(results.data);
-      }
+    if (foundRecipe) {
+      setSelectRecipe(foundRecipe);
+    } else {
+      const results = await axios.get(`/api/recipes/${id}`);
+      setSelectRecipe(results.data);
     }
-  }, [recipes]);
+  }, []);
+
+  if (!selectRecipe) {
+    return null;
+  }
 
   return (
     <>
       <NavBar />
-      {selectRecipe && (
-        <Section>
-          <Container>
-            <Columns>
-              <Columns.Column>
-                <Columns centered>
-                  <Columns.Column size={12}>
-                    <Heading className="has-text-centered">
-                      {selectRecipe.title}
-                    </Heading>
-                  </Columns.Column>
-                  <Columns.Column
-                    className="is-flex is-flex-direction-column is-align-items-center"
-                    size={12}
-                  >
-                    <p>{`Spoonacular score: ${selectRecipe.spoonacularScore}`}</p>
-                    <p>{`Likes: ${selectRecipe.aggregateLikes}`}</p>
-                  </Columns.Column>
-                  <Columns.Column
-                    className="is-flex is-justify-content-center"
-                    size={12}
-                  >
-                    <RecipeImage
-                      src={selectRecipe.image}
-                      alt={selectRecipe.title}
-                    />
-                  </Columns.Column>
-                  <Columns.Column
-                    className="is-flex is-justify-content-center"
-                    size={12}
-                  >
-                    <p
-                      dangerouslySetInnerHTML={{ __html: selectRecipe.summary }}
-                    />
-                  </Columns.Column>
-                  <Columns.Column
-                    className="is-flex is-justify-content-center"
-                    size={12}
-                  >
-                    <div className="is-flex">
-                      <InfoListLeft>
-                        <InfoListItem
-                          type="Vegetarian"
-                          value={selectRecipe.vegetarian}
-                        />
-                        <InfoListItem type="Vegan" value={selectRecipe.vegan} />
-                        <InfoListItem
-                          type="Gluten free"
-                          value={selectRecipe.glutenFree}
-                        />
-                        <InfoListItem
-                          type="Dairy free"
-                          value={selectRecipe.dairyFree}
-                        />
-                      </InfoListLeft>
-                      <InfoList>
-                        <InfoListItem
-                          type="Very healthy"
-                          value={selectRecipe.veryHealthy}
-                        />
-                        <InfoListItem
-                          type="Very popular"
-                          value={selectRecipe.veryPopular}
-                        />
-                        <InfoListItem type="Cheap" value={selectRecipe.cheap} />
-                      </InfoList>
-                    </div>
-                  </Columns.Column>
-                  <Columns.Column size={12}>
-                    <Columns>
-                      {selectRecipe.nutrition.nutrients.map((nutrient) => (
-                        <NutritionItem
-                          nutrient={nutrient}
-                          key={nutrient.name}
-                        />
-                      ))}
-                    </Columns>
-                  </Columns.Column>
-                  <Columns.Column>Test 1</Columns.Column>
-                  <Columns.Column>Test 2</Columns.Column>
-                </Columns>
-              </Columns.Column>
-            </Columns>
-          </Container>
-        </Section>
-      )}
+      <Section>
+        <Container>
+          <Columns>
+            <Columns.Column>
+              <Columns centered>
+                <Columns.Column size={12}>
+                  <Heading className="has-text-centered">
+                    {selectRecipe.title}
+                  </Heading>
+                </Columns.Column>
+                <Columns.Column
+                  className="is-flex is-flex-direction-column is-align-items-center"
+                  size={12}
+                >
+                  <p>{`Spoonacular score: ${selectRecipe.spoonacularScore}`}</p>
+                  <p>{`Likes: ${selectRecipe.aggregateLikes}`}</p>
+                </Columns.Column>
+                <Columns.Column
+                  className="is-flex is-justify-content-center"
+                  size={12}
+                >
+                  <RecipeImage
+                    src={selectRecipe.image}
+                    alt={selectRecipe.title}
+                  />
+                </Columns.Column>
+                <Columns.Column
+                  className="is-flex is-justify-content-center"
+                  size={12}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{ __html: selectRecipe.summary }}
+                  />
+                </Columns.Column>
+                <Columns.Column
+                  className="is-flex is-justify-content-center"
+                  size={12}
+                >
+                  <div className="is-flex">
+                    <InfoListLeft>
+                      <InfoListItem
+                        type="Vegetarian"
+                        value={selectRecipe.vegetarian}
+                      />
+                      <InfoListItem type="Vegan" value={selectRecipe.vegan} />
+                      <InfoListItem
+                        type="Gluten free"
+                        value={selectRecipe.glutenFree}
+                      />
+                      <InfoListItem
+                        type="Dairy free"
+                        value={selectRecipe.dairyFree}
+                      />
+                    </InfoListLeft>
+                    <InfoList>
+                      <InfoListItem
+                        type="Very healthy"
+                        value={selectRecipe.veryHealthy}
+                      />
+                      <InfoListItem
+                        type="Very popular"
+                        value={selectRecipe.veryPopular}
+                      />
+                      <InfoListItem type="Cheap" value={selectRecipe.cheap} />
+                    </InfoList>
+                  </div>
+                </Columns.Column>
+                <Columns.Column size={12}>
+                  <Columns>
+                    {selectRecipe.nutrition.nutrients.map((nutrient) => (
+                      <NutritionItem nutrient={nutrient} key={nutrient.name} />
+                    ))}
+                  </Columns>
+                </Columns.Column>
+                <Columns.Column>Test 1</Columns.Column>
+                <Columns.Column>Test 2</Columns.Column>
+              </Columns>
+            </Columns.Column>
+          </Columns>
+        </Container>
+      </Section>
     </>
   );
 };
