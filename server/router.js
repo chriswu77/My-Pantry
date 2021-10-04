4fd683f946e4329c71cc11edf9916669d845a11d
Chris Wu <cwu0569@gmail.com>
2021-10-04 00:15:09 -0700

    re-factored main router to use different routers

--- server/router.js
+++ server/router.js
@@ -2,39 +2,47 @@ const router = require('express').Router();
 const controllers = require('./controllers');
 const passport = require('./passport/index');
 
-router.get('/users', controllers.getUserId);
-router.post('/users', controllers.createUser);
+const users = require('./routes/users');
+const ingredients = require('./routes/ingredients');
+const recipes = require('./routes/recipes');
 
-router.post(
-  '/users/login',
-  (req, res, next) => {
-    console.log('login route, req.body: ', req.body);
-    next();
-  },
-  passport.authenticate('local'),
-  controllers.login
-);
+router.use('/users', users);
+router.use('/ingredients', ingredients);
+router.use('/recipes', recipes);
 
-router.get('/users/logout', controllers.logout);
+// router.get('/users', controllers.getUserId);
+// router.post('/users', controllers.createUser);
 
-router.post('/ingredients/search', controllers.searchIngredient);
+// router.post(
+//   '/users/login',
+//   (req, res, next) => {
+//     console.log('login route, req.body: ', req.body);
+//     next();
+//   },
+//   passport.authenticate('local'),
+//   controllers.login
+// );
 
-router.post('/recipes/search', controllers.searchRecipes);
+// router.get('/users/logout', controllers.logout);
 
-router.get('/recipes/:recipeId', controllers.getRecipeInfoTest);
+// router.post('/ingredients/search', controllers.searchIngredient);
 
-router.get('/users/:userId', controllers.getUser);
+// router.post('/recipes/search', controllers.searchRecipes);
 
-router.get('/users/:userId/ingredients', controllers.getIngredients);
-// router.post('/users/:userId/ingredients', controllers.addIngredient);
-router.post('/users/:userId/ingredients', controllers.addIngredients);
-router.delete(
-  '/users/:userId/ingredients/:ingredientId',
-  controllers.removeIngredient
-);
+// router.get('/recipes/:recipeId', controllers.getRecipeInfoTest);
 
-router.get('/users/:userId/recipes', controllers.getRecipes);
-router.post('/users/:userId/recipes', controllers.addRecipe);
-router.delete('/users/:userId/recipes/:recipeId', controllers.removeRecipe);
+// router.get('/users/:userId', controllers.getUser);
+
+// router.get('/users/:userId/ingredients', controllers.getIngredients);
+// // router.post('/users/:userId/ingredients', controllers.addIngredient);
+// router.post('/users/:userId/ingredients', controllers.addIngredients);
+// router.delete(
+//   '/users/:userId/ingredients/:ingredientId',
+//   controllers.removeIngredient
+// );
+
+// router.get('/users/:userId/recipes', controllers.getRecipes);
+// router.post('/users/:userId/recipes', controllers.addRecipe);
+// router.delete('/users/:userId/recipes/:recipeId', controllers.removeRecipe);
 
 module.exports = router;
