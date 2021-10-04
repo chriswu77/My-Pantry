a109c78476d13bb1cdf9a7ad311036711393d88d
Chris Wu <cwu0569@gmail.com>
2021-10-03 22:58:44 -0700

    created function to resolve a single recipe

--- server/helperFunctions.js
+++ server/helperFunctions.js
@@ -1,5 +1,8 @@
 const User = require('../database/models/user');
+const Recipe = require('../database/models/recipe');
 const Ingredient = require('../database/models/ingredient');
+const { getRecipeInfo } = require('../apiHelpers/spoonacular');
+const { getOrSetCache } = require('../redis/helpers');
 
 const resolveIngredient = async (ingredient, foundUser) => {
   const foundIngredient = await Ingredient.findOne({
@@ -27,4 +30,28 @@ const resolveIngredient = async (ingredient, foundUser) => {
   }
 };
 
-module.exports = { resolveIngredient };
+const resolveRecipe = async (recipeId, foundUser) => {
+  const foundRecipe = await Recipe.findOne({ id: recipeId });
+
+  if (foundRecipe) {
+    const alreadyHasRecipe = await User.findOne({
+      recipes: foundRecipe._id,
+    });
+
+    if (!alreadyHasRecipe) {
+      foundUser.recipes.push(foundRecipe);
+      await foundUser.save();
+    }
+  } else {
+    const recipeData = await getOrSetCache(`recipes:${recipeId}`, async () => {
+      const data = await getRecipeInfo(recipeId);
+      return data;
+    });
+    const newRecipe = new Recipe(recipeData);
+    await newRecipe.save();
+    foundUser.recipes.push(newRecipe);
+    await foundUser.save();
+  }
+};
+
+module.exports = { resolveIngredient, resolveRecipe };
